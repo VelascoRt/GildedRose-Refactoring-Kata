14a5d3fffa34d8d9bdb9171c6c532597b3409187
VelascoRt <velascogmac@gmail.com>
2026-05-02 20:25:31 -0600

    refactor: replace conditionals with polymorphism (Strategy pattern)

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,64 +1,63 @@
 # -*- coding: utf-8 -*-
 class ItemUpdater:
-    def update(self, item):
-        self._update_sell_in(item)
-        self._update_quality(item)
+    def __init__(self, item):
+        self.item = item
 
-    def _update_sell_in(self, item):
-        item.sell_in -= 1
+    def update(self):
+        raise NotImplementedError
 
-    def _update_quality(self, item):
-        # comportamiento por defecto
-        if item.quality > 0:
-            item.quality -= 1
-        if item.sell_in < 0 and item.quality > 0:
-            item.quality -= 1
+class NormalItemUpdater(ItemUpdater):
+    def update(self):
+        self.item.sell_in -= 1
+        degradation = 2 if self.item.sell_in < 0 else 1
+        self.item.quality = max(0, self.item.quality - degradation)
 
-class GildedRose(object):
+class AgedBrieUpdater(ItemUpdater):
+    def update(self):
+        self.item.sell_in -= 1
+        improvement = 2 if self.item.sell_in < 0 else 1
+        self.item.quality = min(50, self.item.quality + improvement)
 
+class SulfurasUpdater(ItemUpdater):
+    def update(self):
+        pass  # Sulfuras nunca cambia
+
+class BackstagePassUpdater(ItemUpdater):
+    def update(self):
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0:
+            self.item.quality = 0
+        elif self.item.sell_in < 5:
+            self.item.quality = min(50, self.item.quality + 3)
+        elif self.item.sell_in < 10:
+            self.item.quality = min(50, self.item.quality + 2)
+        else:
+            self.item.quality = min(50, self.item.quality + 1)
+
+# Factory para seleccionar el updater correcto
+class UpdaterFactory:
+
+    _registry = {
+        "AGED_BRIE": AgedBrieUpdater,
+        "SULFURAS": SulfurasUpdater,
+        "BACKSTAGE_PASSES": BackstagePassUpdater,
+    }
+
+    @classmethod
+    def for_item(cls, item):
+        updater_class = cls._registry.get(item.name, NormalItemUpdater)
+        return updater_class(item)
+
+# GildedRose simplificada
+class GildedRose:
     def __init__(self, items):
         self.items = items
-        self.AGED_BRIE = "Aged Brie"
-        self.SULFURAS = "Sulfuras, Hand of Ragnaros"
-        self.BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
-    
-
-    def _update_aged_brie(self, item):
-        if item.quality < 50:
-            item.quality += 1
-        item.sell_in -= 1
-        if item.sell_in < 0 and item.quality < 50:
-            item.quality += 1
-
-    def _update_backstage_pass(self, item):
-        if item.quality < 50:
-            item.quality = item.quality + 1
-            if item.name == self.BACKSTAGE_PASSES:
-                if item.sell_in < 11:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-                if item.sell_in < 6:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
-
-    def _update_normal_item(self, item):
-        if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
-            if item.quality > 0:
-                if item.name != self.SULFURAS:
-                    item.quality = item.quality - 1
-
-    def _is_sulfuras(self, item):
-        return item.name == self.SULFURAS
-
 
     def update_quality(self):
         for item in self.items:
-            if self._is_sulfuras(item):
-                return  # no hacer nada
-            self._update_aged_brie(item)
-            self._update_backstage_pass(item)
-            self._update_normal_item(item)
+            UpdaterFactory.for_item(item).update()
+
+#----------------------------------------------------------------
 
 class Item:
     def __init__(self, name, sell_in, quality):
