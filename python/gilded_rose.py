d165e4b41bf7d38a3a2b7cf984167c8e940bb424
VelascoRt <velascogmac@gmail.com>
2026-05-02 18:37:19 -0600

    refactor: extracted method update_backstage_pass

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -16,6 +16,17 @@ class GildedRose(object):
         if item.sell_in < 0 and item.quality < 50:
             item.quality += 1
 
+    def _update_backstage_pass(self, item):
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.name == self.BACKSTAGE_PASSES:
+                if item.sell_in < 11:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
     def update_quality(self):
         for item in self.items:
             if item.name != self.AGED_BRIE and item.name != self.BACKSTAGE_PASSES:
